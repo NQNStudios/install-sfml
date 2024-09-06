3b35688e0a50e4a1c150758ee1a84a24b0986c62
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-09-06 16:30:55 -0500

    use script_dir

--- build-deps.sh
+++ build-deps.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 basic_cmake() {
     dir=$1
-    cmake -DCMAKE_INSTALL_PREFIX=./ -S $dir -B $dir/build
-    (cd $dir/build && make && make install)
+    cmake -DCMAKE_INSTALL_PREFIX=./ -S $SCRIPT_DIR/$dir -B $SCRIPT_DIR/$dir/build
+    (cd $SCRIPT_DIR/$dir/build && make && make install)
 }
 
 basic_cmake freetype
