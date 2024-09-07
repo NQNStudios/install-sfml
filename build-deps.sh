1caae04e8b45d1e872d1c9769d0052bd68b60f50
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-09-06 20:28:52 -0500

    possibly actually work?

--- build-deps.sh
+++ build-deps.sh
@@ -7,10 +7,22 @@ basic_cmake() {
     ARCH=$(uname -m)
 
     cmake $extra -DBUILD_FRAMEWORK=1 -DCMAKE_OSX_ARCHITECTURES="$ARCH" -DINSTALL_MANPAGES=OFF -DCMAKE_INSTALL_PREFIX=./ -S $dir -B $dir/build
-    (cd $dir/build && make && make install) || exit 1
+    (cd $dir/build && (xcodebuild -arch "$ARCH" -configuration "$CONFIGURATION" || make) && make install) || exit 1
 }
 
 basic_cmake ogg
+cp -r Ogg.framework lib/
+
 basic_cmake vorbis "-DBUILD_TESTING=0 -DOGG_ROOT=$(pwd)"
+
+cp -r vorbis/build/lib/Vorbis.framework lib/
+
 basic_cmake flac "-DOGG_ROOT=$(pwd)"
-basic_cmake freetype "-GXcode -DCMAKE_GENERATOR=Xcode"
+
+flags=""
+if [ "$(uname)" = "Darwin" ]; then
+    flags="-GXcode -DCMAKE_GENERATOR=Xcode"
+fi
+
+basic_cmake freetype $flags
+cp -r Library/Frameworks/freetype.framework lib/
