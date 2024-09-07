d8b8fc9044bcaf246037a9e017588631e42bbcfa
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-09-06 20:08:01 -0500

    builds ogg, vorbis, flac

--- build-deps.sh
+++ build-deps.sh
@@ -2,11 +2,15 @@
 
 basic_cmake() {
     dir=$1
-    cmake DINSTALL_MANPAGES=OFF -DCMAKE_INSTALL_PREFIX=./ -S $dir -B $dir/build
-    (cd $dir/build && make && make install)
+    extra=$2
+
+    ARCH=$(uname -m)
+
+    cmake $extra -DBUILD_FRAMEWORK=1 -DCMAKE_OSX_ARCHITECTURES="$ARCH" -DINSTALL_MANPAGES=OFF -DCMAKE_INSTALL_PREFIX=./ -S $dir -B $dir/build
+    (cd $dir/build && make && make install) || exit 1
 }
 
 basic_cmake ogg
-basic_cmake vorbis
-basic_cmake flac
-basic_cmake freetype
+basic_cmake vorbis "-DBUILD_TESTING=0 -DOGG_ROOT=$(pwd)"
+basic_cmake flac "-DOGG_ROOT=$(pwd)"
+basic_cmake freetype "-GXcode -DCMAKE_GENERATOR=Xcode"
